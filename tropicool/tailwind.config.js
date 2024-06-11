7d6001ea0694b931db20978897049f478de6d930
Phang Willy <pro.phang.willy@gmail.com>
2024-06-12 00:24:49 +0200

    admin dashboard

--- tropicool/tailwind.config.js
+++ tropicool/tailwind.config.js
@@ -6,9 +6,8 @@ module.exports = {
   theme: {
     extend: {
       colors : {
-        'cstm-orange' : '#F6AE20',
-        'cstm-green' : '#42C686',
-        'cstm-purple' : '#9644FF'
+        'main' : '#696BE2',
+        'secondary' : '#1D1F96',
       }
     },
   },
