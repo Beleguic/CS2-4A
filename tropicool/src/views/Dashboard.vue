6550b68c5e61830307c98a5e8a13e5b11a31b031
Phang Willy <pro.phang.willy@gmail.com>
2024-06-12 00:24:49 +0200

    admin dashboard

--- tropicool/src/views/Dashboard.vue
+++ tropicool/src/views/Dashboard.vue
@@ -1,28 +1,11 @@
 <template>
-    <div class="dashboard">
-        <h1>Tableau de Bord</h1>
-        <p>Bienvenue sur votre tableau de bord. Ici, vous pouvez visualiser les informations et les statistiques importantes.</p>
-        <!-- Ajoutez plus de contenu ici selon les besoins -->
-    </div>
+    <section class="h-full"></section>
 </template>
 
 <script setup>
-// Logique spécifique au tableau de bord si nécessaire
 
 </script>
 
 <style scoped>
-    .dashboard {
-    padding: 20px;
-    background-color: #f4f4f4;
-    border-radius: 10px;
-    }
 
-    h1 {
-    color: #333;
-    }
-
-    p {
-    color: #666;
-    }
 </style>
