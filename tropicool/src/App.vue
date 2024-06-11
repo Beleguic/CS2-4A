1d879b57a8bb40a6f2d304b042cb223d0aea673f
Phang Willy <pro.phang.willy@gmail.com>
2024-06-12 00:24:49 +0200

    admin dashboard

--- tropicool/src/App.vue
+++ tropicool/src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
-    <Navbar />
-    <main>
-      <router-view></router-view>
-    </main>
-    <FooterComponent />
+    <div v-if="isDashboardRoute" class="h-screen flex">
+      <DashboardSidebar v-if="isDashboardRoute" />
+      <main class="bg-white p-0 flex-1 flex flex-col ml-80 relative">
+        <DashboardNavbar />
+        <router-view></router-view>
+      </main>
+    </div>
+
+    <Navbar v-if="!isDashboardRoute"/>
+    <main v-if="!isDashboardRoute">
+        <router-view></router-view>
+      </main>
+    <FooterComponent v-if="!isDashboardRoute"/>
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
 import Navbar from './components/NavbarComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
+import DashboardSidebar from './components/dashboard/Sidebar.vue';
+import DashboardNavbar from './components/dashboard/Navbar.vue';
+
+const route = useRoute();
+
+const isDashboardRoute = computed(() => route.path.startsWith('/dashboard'));
 </script>
 
 <style>
@@ -18,6 +35,7 @@ import FooterComponent from './components/FooterComponent.vue';
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+  height: 100%;
 }
 
 main {
