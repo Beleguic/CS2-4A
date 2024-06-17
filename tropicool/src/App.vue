ce236f6cd037c5d3500aba5a7add5d6e69c212d8
Phang Willy <pro.phang.willy@gmail.com>
2024-06-18 00:02:57 +0200

    delete package-lock.json

--- tropicool/src/App.vue
+++ tropicool/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div v-if="isDashboardRoute" class="h-screen flex">
       <DashboardSidebar v-if="isDashboardRoute" />
-      <main class="bg-white p-0 flex-1 flex flex-col ml-80 relative">
+      <main id="main-dashboard" class="bg-white p-0 flex-1 flex flex-col ml-80 relative">
         <DashboardNavbar />
         <router-view></router-view>
       </main>
@@ -42,5 +42,10 @@ main {
   flex: 1;
   display: flex;
   flex-direction: column;
+}
+
+main:not(#main-dashboard) {
+  justify-content: center;
+  align-items: center;
 }
 </style>
