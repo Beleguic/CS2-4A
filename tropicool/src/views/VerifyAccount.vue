42b21e6cb19142c2ee9cc9c47fe8fac293f35ecf
Beleguic <thibault.beleguic2@gmail.com>
2024-07-08 23:48:02 +0200

    Change form resetPassword and change CSS of verifyAccount

--- tropicool/src/views/VerifyAccount.vue
+++ tropicool/src/views/VerifyAccount.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
-      <h1>Verification du Compte</h1>
-      <p v-if="verified">Votre compte a été vérifié avec succès!</p>
-      <p v-else>Échec de la vérification de votre compte. Veuillez réessayer ou contacter le support.</p>
+    <div class="relative w-full bg-custom">
+        <!-- Formulaire de reinistialisation de MDP -->
+        <div class="flex flex-col items-center gap-4">
+            <img src="/logo-troupicool.png" class="w-40">
+            <div class="w-full max-w-2xl bg-[rgb(105,107,226)] p-8 rounded-3xl shadow-lg mb-16">
+                <h2 class="text-2xl font-bold mb-1 text-center text-white">Verification du Compte</h2>
+                <p v-if="verified">Votre compte a été vérifié avec succès!</p>
+                <p v-else>Échec de la vérification de votre compte. Veuillez réessayer ou contacter le support.</p>
+            </div>
+        </div>
     </div>
   </template>
   
