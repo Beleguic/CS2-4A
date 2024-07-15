fd9def54a2592558746e488ecb94540dfc0eb60a
Beleguic <thibault.beleguic2@gmail.com>
2024-07-16 00:46:07 +0200

    Finish API LA POSTE (Troupicool Delivery)

--- poste/routes/livraison.js
+++ poste/routes/livraison.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const livraisonController = require('../controllers/livraisonController');
+const livraisonController = require('../controllers/livraisonController.js');
 
+router.get('/', livraisonController.getAllLivraison);
 router.get('/:id', livraisonController.getLivraisonById);
 router.post('/new', livraisonController.createLivraison);
 router.patch('/:id', livraisonController.updateLivraisonStatus);
