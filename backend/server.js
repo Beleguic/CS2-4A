223919a4b8ee4dd5cedff80aa7e227255c74ebd1
clement-Yvars <clement.yvars2001@gmail.com>
2024-07-17 01:31:38 +0200

    refonte image crud + integration card

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require("express");
+const path = require('path');
 const session = require('express-session');
 const cookieParser = require("cookie-parser");
 const UserRouter = require("./routes/user");
@@ -21,24 +22,23 @@ const cors = require("cors");
 const nodemailer = require('nodemailer');
 const app = express();
 
-// Configuration de Nodemailer
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,  // true for 465, false for other ports
+    secure: false, 
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD
     }
 });
 
-// Middleware pour rendre le transporter disponible dans les requêtes
+
 app.use((req, res, next) => {
     req.transporter = transporter;
     next();
 });
 
-// Configuration de la session
+
 app.use(session({
     secret: 'challenge4IWS2',
     resave: false,
@@ -46,12 +46,15 @@ app.use(session({
     cookie: { secure: process.env.NODE_ENV === 'production' }
 }));
 
-// Middlewares standards pour le parsing des cookies et des JSON bodies
+
 app.use(cookieParser(process.env.JWT_SECRET));
-app.use(express.json());  // Assurez-vous que ce middleware est présent
+app.use(express.json());
 app.use(cors());
 
-// Utilisation du router pour les utilisateurs
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
 app.use("/users", UserRouter);
 app.use("/auth", AuthRouter);
 app.use("/category", CategoryRouter);
@@ -67,7 +70,8 @@ app.use('/user_history', UserHistoryRouter);
 app.use('/password_history', PasswordHistoryRouter);
 app.use('/product_promotion', ProductPromotionRouter);
 app.use('/promotion_code', PromotionRouter);
-// Démarrage du serveur
+
+
 app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT);
 });
