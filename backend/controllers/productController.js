7e9eec91e79d7addbe29f3e081637a934f521e93
clement-Yvars <clement.yvars2001@gmail.com>
2024-07-17 01:31:38 +0200

    refonte image crud + integration card

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,7 @@ const Joi = require('joi');
 const productSchema = Joi.object({
   name: Joi.string().min(3).max(255).required(),
   price: Joi.number().greater(0).required(),
-  image: Joi.string().uri().required(),
+  image: Joi.string().optional(), // L'image devient optionnelle car elle sera gérée par Multer
   is_active: Joi.boolean().optional(),
   description: Joi.string().min(3).required(),
   is_adult: Joi.boolean().optional()
@@ -52,13 +52,27 @@ const getAllProductsForSelection = async (req, res, next) => {
 
 const getAllProducts = async (req, res, next) => {
   try {
-    const products = await Product.findAll();
-    res.json(products);
+    const page = parseInt(req.query.page) || 1;  
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
+
+    const { count, rows } = await Product.findAndCountAll({
+      offset: offset,
+      limit: limit
+    });
+
+    res.json({
+      totalItems: count,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+      products: rows
+    });
   } catch (e) {
     console.error('Error fetching products:', e);
     next(e);
   }
 };
+
 
 const getProductById = async (req, res, next) => {
   try {
@@ -83,7 +97,13 @@ const createProduct = async (req, res, next) => {
       return res.status(400).json({ error: error.details[0].message });
     }
 
-    const product = await Product.create(req.body);
+    const image = req.file ? req.file.path : null;
+    const newProductData = {
+      ...req.body,
+      image,
+    };
+
+    const product = await Product.create(newProductData);
     res.status(201).json(product);
   } catch (e) {
     console.error('Error creating product:', e);
@@ -102,7 +122,8 @@ const updateProduct = async (req, res, next) => {
 
     const product = await Product.findByPk(req.params.id);
     if (product) {
-      await product.update(payload);
+      const image = req.file ? req.file.path : product.image; // Conserve l'ancienne image si une nouvelle n'est pas fournie
+      await product.update({ ...payload, image });
       res.json(product);
     } else {
       res.sendStatus(404);
