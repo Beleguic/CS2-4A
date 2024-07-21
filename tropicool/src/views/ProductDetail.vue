5b6125c4209a72551f6d059af90cd1c65c7a3b5f
clement-Yvars <clement.yvars2001@gmail.com>
2024-07-21 19:37:47 +0200

    toast panier

--- tropicool/src/views/ProductDetail.vue
+++ tropicool/src/views/ProductDetail.vue
@@ -10,10 +10,12 @@
         <AddToCart
           :item="product.id"
           :price="product.price"
+          @item-added="handleItemAdded"
         />
         <p v-if="product.is_adult" class="alcohol-warning">Contient de l'alcool. À consommer avec modération.</p>
       </div>
     </div>
+    <ToastManager ref="toastManager" />
   </div>
   <div v-else class="access-denied">
     <p class="main-message">Vous devez être majeur pour accéder à cette page.</p>
@@ -25,215 +27,214 @@
 </template>
 
 <script setup>
-  import { ref, computed, onMounted } from 'vue';
-  import { useAuthStore } from '@/stores/authStore'; 
-  import { useRouter } from 'vue-router';
-  import axios from 'axios';
-  import AddToCart from './AddToCart.vue';
+import { ref, computed, onMounted } from 'vue';
+import { useAuthStore } from '@/stores/authStore'; 
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+import AddToCart from '../views/AddToCart.vue'; // Chemin relatif à partir de src/views
+import ToastManager from '../components/ToastManager.vue'; // Chemin relatif à partir de src/views
 
-  const product = ref({
-    id: '',
-    price: 0,
-    name: '',
-    description: '',
-    image: '',
-    is_adult: false
-  });
-  const quantity = ref(1);
-  const isAllowed = ref(true);
-  const router = useRouter();
+const product = ref({
+  id: '',
+  price: 0,
+  name: '',
+  description: '',
+  image: '',
+  is_adult: false
+});
+const isAllowed = ref(true);
+const router = useRouter();
+const toastManager = ref(null);
 
-  const authStore = useAuthStore();
+const authStore = useAuthStore();
 
-  const fetchProduct = async () => {
-    const productId = router.currentRoute.value.params.id;
+const fetchProduct = async () => {
+  const productId = router.currentRoute.value.params.id;
+  const apiUrl = import.meta.env.VITE_API_URL;
+  try {
+    const response = await fetch(`${apiUrl}/product/${productId}?frontend=true`);
+    const data = await response.json();
+    product.value = data;
+    if (data.is_adult) {
+      checkAge();
+    }
+  } catch (error) {
+    console.error('Error fetching product:', error);
+  }
+};
+
+const checkAge = async () => {
+  if (authStore.userId) {
     const apiUrl = import.meta.env.VITE_API_URL;
     try {
-      const response = await fetch(`${apiUrl}/product/${productId}?frontend=true`);
-      const data = await response.json();
-      product.value = data;
-      if (data.is_adult) {
-        checkAge();
+      const response = await axios.get(`${apiUrl}/users/${authStore.userId}`);
+      const user = response.data;
+      const age = calculateAge(new Date(user.dateOfBirth));
+      if (age < 18) {
+        isAllowed.value = false;
+      } else {
+        isAllowed.value = true;
       }
     } catch (error) {
-      console.error('Error fetching product:', error);
-    }
-  };
-
-  const checkAge = async () => {
-    if (authStore.userId) {
-      const apiUrl = import.meta.env.VITE_API_URL;
-      try {
-        const response = await axios.get(`${apiUrl}/users/${authStore.userId}`);
-        const user = response.data;
-        console.log('User data:', user);
-        const age = calculateAge(new Date(user.dateOfBirth));
-        console.log('User age:', age);
-        if (age < 18) {
-          isAllowed.value = false;
-          console.log('Access denied: User is not an adult.');
-        } else {
-          isAllowed.value = true;
-          console.log('Access granted: User is an adult.');
-        }
-      } catch (error) {
-        console.error('Error fetching user:', error);
-        isAllowed.value = false;
-      }
-    } else {
+      console.error('Error fetching user:', error);
       isAllowed.value = false;
-      console.log('Access denied: No user ID found.');
     }
-  };
+  } else {
+    isAllowed.value = false;
+  }
+};
 
-  const calculateAge = (birthdate) => {
-    const today = new Date();
-    let age = today.getFullYear() - birthdate.getFullYear();
-    const monthDifference = today.getMonth() - birthdate.getMonth();
-    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthdate.getDate())) {
-      age--;
-    }
-    console.log('Calculated age:', age);
-    return age;
-  };
+const calculateAge = (birthdate) => {
+  const today = new Date();
+  let age = today.getFullYear() - birthdate.getFullYear();
+  const monthDifference = today.getMonth() - birthdate.getMonth();
+  if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthdate.getDate())) {
+    age--;
+  }
+  return age;
+};
 
-  onMounted(() => {
-    fetchProduct();
-  });
+const handleItemAdded = (itemId) => {
+  toastManager.value.addToast("L'article a été ajouté à votre panier.", 'info');
+};
+
+onMounted(() => {
+  fetchProduct();
+});
 </script>
 
 <style scoped>
-  body {
-    background-color: #FEFEF6;
-    margin: 0;
-    font-family: Arial, sans-serif;
-  }
+body {
+  background-color: #FEFEF6;
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
 
-  .page-container {
-    background-color: #FEFEF6;
-    min-height: 100vh;
-  }
+.page-container {
+  background-color: #FEFEF6;
+  min-height: 100vh;
+}
 
-  .product-page {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 40px;
-    background-color: #FEFEF6;
-    min-height: 100vh;
-    width: 100%;
-    box-sizing: border-box;
-  }
+.product-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 40px;
+  background-color: #FEFEF6;
+  min-height: 100vh;
+  width: 100%;
+  box-sizing: border-box;
+}
 
-  .product-container {
-    display: flex;
-    align-items: center;
-    background-color: #fff;
-    padding: 40px;
-    border-radius: 15px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    max-width: 1200px;
-    width: 80%; /* Ajustez cette valeur pour augmenter la largeur */
-    min-height: 600px;
-    box-sizing: border-box;
-  }
+.product-container {
+  display: flex;
+  align-items: center;
+  background-color: #fff;
+  padding: 40px;
+  border-radius: 15px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  max-width: 1200px;
+  width: 80%; /* Ajustez cette valeur pour augmenter la largeur */
+  min-height: 600px;
+  box-sizing: border-box;
+}
 
-  .product-image {
-    flex: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.product-image {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  .product-image img {
-    width: 100%;
-    max-width: 400px;
-    height: auto;
-    border-radius: 10px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  }
+.product-image img {
+  width: 100%;
+  max-width: 400px;
+  height: auto;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
 
-  .product-details {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
+.product-details {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
 
-  .product-name {
-    font-size: 36px;
-    font-weight: bold;
-    color: #696BE2;
-    margin-bottom: 20px;
-  }
+.product-name {
+  font-size: 36px;
+  font-weight: bold;
+  color: #696BE2;
+  margin-bottom: 20px;
+}
 
-  .product-description {
-    font-size: 20px;
-    color: #4A4A4A;
-    margin-bottom: 30px;
-  }
+.product-description {
+  font-size: 20px;
+  color: #4A4A4A;
+  margin-bottom: 30px;
+}
 
-  .age-verification {
-    font-size: 20px;
-    color: red;
-    margin-bottom: 30px;
-  }
+.age-verification {
+  font-size: 20px;
+  color: red;
+  margin-bottom: 30px;
+}
 
-  .verify-age-button {
-    background-color: #696BE2;
-    color: #FEFEF6;
-    font-size: 18px;
-    font-weight: 500;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 10px;
-    cursor: pointer;
-    transition: background-color 0.3s;
-  }
+.verify-age-button {
+  background-color: #696BE2;
+  color: #FEFEF6;
+  font-size: 18px;
+  font-weight: 500;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
 
-  .verify-age-button:hover {
-    background-color: #5756A1;
-  }
+.verify-age-button:hover {
+  background-color: #5756A1;
+}
 
-  .product-price {
-    font-size: 22px;
-    font-weight: 500;
-    color: #1D1F96;
-    margin-bottom: 30px;
-  }
+.product-price {
+  font-size: 22px;
+  font-weight: 500;
+  color: #1D1F96;
+  margin-bottom: 30px;
+}
 
-  .alcohol-warning {
-    color: red;
-    font-size: 16px;
-    margin-bottom: 20px;
-  }
+.alcohol-warning {
+  color: red;
+  font-size: 16px;
+  margin-bottom: 20px;
+}
 
-  .access-denied {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 40px;
-    background-color: #FEFEF6;
-    min-height: 100vh;
-    width: 100%;
-    box-sizing: border-box;
-    text-align: center;
-    font-size: 24px;
-    color: red;
-  }
+.access-denied {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 40px;
+  background-color: #FEFEF6;
+  min-height: 100vh;
+  width: 100%;
+  box-sizing: border-box;
+  text-align: center;
+  font-size: 24px;
+  color: red;
+}
 
-  .access-denied .main-message {
-    font-size: 24px;
-    font-weight: bold;
-    margin-bottom: 20px;
-  }
+.access-denied .main-message {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
 
-  .access-denied .secondary-message {
-    font-size: 18px;
-    line-height: 1.5;
-    max-width: 800px;
-  }
+.access-denied .secondary-message {
+  font-size: 18px;
+  line-height: 1.5;
+  max-width: 800px;
+}
 </style>
