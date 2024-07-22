e769c53cd2bbc2b15daa01ee64c5a72bfcfe5241
Thibault Beleguic <thibault.beleguic2@gmail.com>
2024-07-22 15:19:05 +0200

    Add view and Add to Stocl Keeper

--- backend/routes/stock.js
+++ backend/routes/stock.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const stockController = require('../controllers/stockController');
 
 router.get('/', stockController.getAllStocks);
+router.get('/store-keeper/:product_id', stockController.getStockByIdForStoreKeeper);
+router.get('/store-keeper', stockController.getAllStocksForStoreKeeper);
 router.get('/:id', stockController.getStockById);
 router.post('/new', stockController.createStock);
 router.patch('/:id', stockController.updateStock);
