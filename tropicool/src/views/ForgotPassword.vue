b22ac1bc4ab45020ac864d312f54caf30fa6cc6e
Matiss Haillouy <matiss.haillouy@gmail.com>
2024-07-23 15:55:21 +0200

    fix login and ban account 2h change formulaire login link to forgot password and orthographe s'incrire

--- tropicool/src/views/ForgotPassword.vue
+++ tropicool/src/views/ForgotPassword.vue
@@ -1,20 +1,19 @@
 <template>
-    <div class="relative w-full bg-custom">
-        <!-- Formulaire de reinistialisation de MDP -->
-        <div class="flex flex-col items-center gap-4">
-            <img src="/logo-troupicool.png" class="w-40">
-            <div class="w-full max-w-2xl bg-[rgb(105,107,226)] p-8 rounded-3xl shadow-lg mb-16">
-                <h2 class="text-2xl font-bold mb-1 text-center text-white">Mot de passe oublié</h2>
-                <FormComponent
-                    :fields="fields"
-                    submitButtonText="Envoyer le lien de réinitialisation"
-                    @submit.prevent="submit"
-                />
-            </div>
-        </div>
+  <div class="relative w-full bg-custom">
+    <div class="flex flex-col items-center gap-4">
+      <img src="/logo-troupicool.png" class="w-40">
+      <div class="w-full max-w-2xl bg-[rgb(105,107,226)] p-8 rounded-3xl shadow-lg mb-16">
+        <h2 class="text-2xl font-bold mb-1 text-center text-white">Mot de passe oublié</h2>
+        <FormComponent
+          :fields="fields"
+          submitButtonText="Envoyer le lien de réinitialisation"
+          @submit="handleSubmit"
+        />
+      </div>
     </div>
+  </div>
 </template>
-      
+
 <script setup>
 import { ref } from 'vue';
 import FormComponent from '../components/FormComponent.vue';
@@ -22,21 +21,21 @@ import FormComponent from '../components/FormComponent.vue';
 const email = ref('');
 
 const fields = [
-    {  
+  {
     field: [
-        [{ name: 'email', label: 'Adresse mail', type: 'email', required: true, color: 'white' }],
-      ],
-    },
+      [{ name: 'email', label: 'Adresse mail', type: 'email', required: true, color: 'white' }],
+    ],
+  },
 ];
 
-const submit = async () => {
+const handleSubmit = async (formData) => {
   try {
     const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/forgot-password`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email: email.value }),
+      body: JSON.stringify({ email: formData.email }),
     });
 
     if (!response.ok) {
@@ -50,7 +49,6 @@ const submit = async () => {
   }
 };
 </script>
-
 
 <style scoped>
 /* Style your form here */
